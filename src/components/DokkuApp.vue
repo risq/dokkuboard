71fe43dd47b9794a521edd1db2959394f6ef41cc
Valentin Ledrapier <valentin.ledrapier@gmail.com>
2016-07-06 16:21:00 +0200

    Handle missing config & url for an app

--- src/components/DokkuApp.vue
+++ src/components/DokkuApp.vue
@@ -5,15 +5,22 @@
     </h2>
     <pulse-loader v-if="isLoading"></pulse-loader>
     <div v-if="!isLoading">
-      <a href="{{url}}">{{url}}</a>
-      <div v-if="config">
-        <h3>config</h3>
-        <table>
-          <tr v-for="(key, value) in config">
-            <th>{{key}}</th>
-            <td>{{value}}</td>
-          </tr>
-        </table>
+      <div class="dokku-app_info">
+        <h3>url:</h3>
+        <a href="{{url}}" v-if="url">{{url}}</a>
+        <i v-if="!url">no url found for app {{name}}</i>
+        <h3>config:</h3>
+        <div v-if="config">
+          <table>
+            <tr v-for="(key, value) in config">
+              <th>{{key}}</th>
+              <td>{{value}}</td>
+            </tr>
+          </table>
+        </div>
+        <div v-if="!config">
+          <i>no config found for app {{name}}</i>
+        </div>
       </div>
       <button v-on:click="deleteApp">Delete</button>
     </div>
@@ -82,12 +89,15 @@ h3 {
 
 table {
   font-family: monospace;
-  margin-bottom: 24px;
 }
 
 hr {
   border-bottom: 1px solid #ddd;
   border-top: none;
   margin: 32px 16px;
+}
+
+.dokku-app_info {
+  margin-bottom: 24px;
 }
 </style>
