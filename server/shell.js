323790038137699cb7325cd22fefb9b1d64a4c35
Valentin Ledrapier <valentin.ledrapier@gmail.com>
2016-07-08 11:52:09 +0200

    Improve server side shell session handling

--- server/shell.js
+++ server/shell.js
@@ -3,49 +3,69 @@ const pty = require('pty.js');
 
 const dokkuConfig = require('../config/dokku');
 
+function getCommand(appName) {
+  const args = [
+    '-t',
+    `-i ${dokkuConfig.privateKey}`,
+    `${dokkuConfig.username}@${dokkuConfig.host}`,
+    `-p ${dokkuConfig.port}`,
+    '--',
+    `enter ${appName}`,
+  ];
+
+  return `ssh ${args.join(' ')} #\n`;
+}
+
+function delayKillProcess(pid, delay = 10000) {
+  return setTimeout(() => {
+    try {
+      console.log(`Killing process ${pid}`);
+      process.kill(pid);
+    } catch (err) {
+      console.log(err);
+    }
+  }, delay);
+}
+
+function initShell(appName, ws) {
+  let connState = 0;
+
+  console.log(`Creating new shell for app ${appName}`);
+  const term = pty.spawn('bash', [], {
+    name: `${appName}-container`,
+    cols: 80,
+    rows: 24,
+    cwd: process.env.PWD,
+    env: process.env,
+  });
+
+  term.on('data', data => {
+    if (connState > 1) {
+      if (ws.readyState === ws.OPEN) {
+        ws.send(data);
+      }
+    } else if (/#/.exec(data)) {
+      connState++;
+    }
+  });
+  ws.on('message', message => term.write(message));
+  ws.on('close', () => {
+    console.log(`Closing existing shell for app ${appName}`);
+    term.write('exit\n');
+    term.end();
+    delayKillProcess(term.pid);
+  });
+
+  term.write(getCommand(appName));
+}
+
 module.exports = {
   init(app) {
     expressWs(app);
 
     app.ws('/shell', (ws, req, next) => {
       if (req.query.appName) {
-        console.log(`Creating new shell for app ${req.query.appName}`);
-        const args = [
-          '-t',
-          `-i ${dokkuConfig.privateKey}`,
-          `${dokkuConfig.username}@${dokkuConfig.host}`,
-          `-p ${dokkuConfig.port}`,
-          '--',
-          `enter ${req.query.appName}`,
-        ];
-        console.log(`command: ssh ${args.join(' ')}`);
-        const term = pty.spawn('ssh', args, {
-          name: `${req.query.appName}-container`,
-          cols: 80,
-          rows: 24,
-          cwd: process.env.PWD,
-          env: process.env,
-        });
-
-        term.on('data', data => {
-          if (ws.readyState === ws.OPEN) {
-            ws.send(data);
-          } else {
-            console.log('connection closed');
-          }
-        });
-        ws.on('message', msg => term.write(msg));
-        ws.on('close', () => {
-          console.log('Closing shell connection - killing PID', term.pid);
-          term.write('exit\n');
-          setTimeout(() => {
-            try {
-              process.kill(term.pid);
-            } catch (err) {
-              console.log(err);
-            }
-          }, 10000);
-        });
+        initShell(req.query.appName, ws);
       }
       next();
     });
